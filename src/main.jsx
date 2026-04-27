338a7d6b271cbd60fedafc9390e28cbdd64446d3
Maqsood Ahmed <81217827+maqsood-jan@users.noreply.github.com>
2026-04-27 23:21:32 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './ChickenFlow_v2.jsx'
+import AppRoot from './ChickenFlow_v2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <AppRoot />
 )
